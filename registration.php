0cb504ef423c6e164ee69047f51b0631e0df81ed
Wishusucess <90965189+Wishusucess@users.noreply.github.com>
2022-01-07 23:51:55 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created By : Hemant Kumar Singh Magento 2x Developer
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Wishusucess_OwlSlider',
+    __DIR__
+);
---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created By : Hemant Kumar Singh Magento 2x Developer
+ */
+var config = {
+    paths: {
+        owlcarousel: "Wishusucess_OwlSlider/js/owl.carousel"
+    },
+    shim: {
+        owlcarousel: {
+            deps: ['jquery']
+        }
+    }
+};
